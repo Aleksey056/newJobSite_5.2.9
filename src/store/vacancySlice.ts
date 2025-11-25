e164dde8b3351c0c5c336b43137c642ca5cedc50
Aleksey Krendukov <aikrendukov@yandex.ru>
2025-11-25 14:04:00 +0500

    update vacancySlice

--- src/store/vacancySlice.ts
+++ src/store/vacancySlice.ts
@@ -7,21 +7,14 @@ export const vacancyFetch = createAsyncThunk(
 	async ({ searchText = '', searchCity = '', page }: vacancyFetchType, { rejectWithValue }) => { // 
 		try {
 			const param = new URLSearchParams({
-				industry: '7',
-				professional_role: '96',
-				page: page.toString(),
-				per_page: '10',
+				industry: '7', professional_role: '96', per_page: '10',
 			})
-
 			if (searchText) param.append('text', searchText)
 			if (searchCity && searchCity !== '') param.append('area', searchCity)
-			// if (searchSkills) param.append('key_skills', searchSkills.join(',')) // не нашел как вытягивать
-
+			if (page !== undefined) param.append('page', page.toString())
 			const url = `https://api.hh.ru/vacancies?${param.toString()}`
-
 			const response = await fetch(url);
 			if (!response.ok) throw new Error('Ошибка при получении данных с HH')
-
 			const data = await response.json();
 			return data;
 		}
@@ -36,9 +29,8 @@ export const fetchVacanciById = createAsyncThunk(
 	async (id: string, { rejectWithValue }) => {
 		try {
 			const response = await fetch(`https://api.hh.ru/vacancies/${id}`)
-			if (!response.ok) throw new Error("Ошибка в получении данных");
+			if (!response.ok) throw new Error("Ошибка при получении данных c HH");
 			const data = await response.json()
-
 			const result = {
 				...data,
 				snippet: {
@@ -46,7 +38,6 @@ export const fetchVacanciById = createAsyncThunk(
 					responsibility: data.snippet?.responsibility || data.description || 'Не указано',
 				},
 			}
-
 			return result;
 		} catch (error: any) {
 			return rejectWithValue(error.message)
