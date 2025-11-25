f224b9aae6c99b93c45c96226ff4294bc60193b8
Aleksey Krendukov <aikrendukov@yandex.ru>
2025-11-25 14:02:23 +0500

    update App

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,16 @@ import Header from './components/Header/Header'
 import { Navigate, Route, Routes } from 'react-router';
 import VacancyPage from './components/VacancyPage/VacancyPage';
 import HomePage from './components/HomePage/HomePage';
+// import VacancyList from './components/VacancyList/VacancyList';
 
 function App() {
 	return (
 		<>
 			<Header />
 			<Routes>
-				<Route path='/vacancies' element={<HomePage />} />
+				<Route path='/' element={<Navigate to='vacancies/moscow' replace />} />
+				<Route path='vacancies/moscow' element={<HomePage city='1' />} />
+				<Route path='vacancies/petersburg' element={<HomePage city='2' />} />
 				<Route path='/vacancies/:id' element={<VacancyPage />} />
 				<Route path='*' element={<Navigate to='/vacancies' replace />} />
 			</Routes>
